cfcc5244254b6374df250dd75bb66f7f488b69a5
Dikshita621 <dikshitachoudhary845@gmail.com>
2026-02-06 00:06:32 +0530

    Add array reversal functionality
    Implement in-place array reversal using two-pointer technique.

--- Day4.c
+++ Day4.c
@@ -1,1 +1,32 @@
+/*Problem: Given an array of n integers, reverse the array in-place using two-pointer approach.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the reversed array, space-separated
+
+Example:
+Input:
+5
+1 2 3 4 5
+
+Output:
+5 4 3 2 1
+
+Explanation: Swap pairs from both ends: (1,5), (2,4), middle 3 stays*/
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+left = 0
+right = n - 1
+
+while left < right:
+    arr[left], arr[right] = arr[right], arr[left]
+    left += 1
+    right -= 1
+
+print(' '.join(map(str, arr)))
 
