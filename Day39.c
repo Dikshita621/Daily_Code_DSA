657f2199b975b427eba34b5bfc2a5e1f8ebce567
Dikshita621 <dikshitachoudhary845@gmail.com>
2026-03-30 10:35:08 +0530

    Create Day39.c

---
+++ Day39.c
@@ -0,0 +1,126 @@
+/*Problem Statement:
+Implement a Min Heap using an array where the smallest element is always at the root.
+
+Supported Operations:
+- insert x
+- extractMin
+- peek
+
+Input Format:
+- First line contains integer N
+- Next N lines contain heap operations
+
+Output Format:
+- Print results of extractMin and peek
+- Print -1 if operation cannot be performed
+
+Example:
+Input:
+6
+insert 40
+insert 10
+insert 30
+peek
+extractMin
+peek
+
+Output:
+10
+10
+30*/
+#include <stdio.h>
+#define MAX 100
+
+int heap[MAX];
+int size = 0;
+
+// Swap function
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Heapify Up
+void heapifyUp(int i) {
+    while (i != 0 && heap[(i - 1) / 2] > heap[i]) {
+        swap(&heap[i], &heap[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+}
+
+// Heapify Down
+void heapifyDown(int i) {
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && heap[left] < heap[smallest])
+        smallest = left;
+
+    if (right < size && heap[right] < heap[smallest])
+        smallest = right;
+
+    if (smallest != i) {
+        swap(&heap[i], &heap[smallest]);
+        heapifyDown(smallest);
+    }
+}
+
+// Insert
+void insert(int x) {
+    if (size == MAX) return;
+
+    heap[size] = x;
+    heapifyUp(size);
+    size++;
+}
+
+// Extract Min
+void extractMin() {
+    if (size == 0) {
+        printf("-1\n");
+        return;
+    }
+
+    printf("%d\n", heap[0]);
+
+    heap[0] = heap[size - 1];
+    size--;
+
+    heapifyDown(0);
+}
+
+// Peek
+void peek() {
+    if (size == 0) {
+        printf("-1\n");
+        return;
+    }
+    printf("%d\n", heap[0]);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char op[20];
+    int x;
+
+    while (n--) {
+        scanf("%s", op);
+
+        if (op[0] == 'i') { // insert
+            scanf("%d", &x);
+            insert(x);
+        }
+        else if (op[0] == 'e') { // extractMin
+            extractMin();
+        }
+        else if (op[0] == 'p') { // peek
+            peek();
+        }
+    }
+
+    return 0;
+}
