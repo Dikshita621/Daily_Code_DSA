23c0b78cd99deb2786a9fd65a4ef6c8ee4d6680e
Dikshita621 <dikshitachoudhary845@gmail.com>
2026-03-30 10:20:51 +0530

    Add infix to postfix conversion program
    Implement infix to postfix conversion using stack.

---
+++ Day33.c
@@ -0,0 +1,101 @@
+/*Problem: Convert an infix expression to postfix notation using stack.
+
+Input:
+- Single line: infix expression (operands are single characters)
+
+Output:
+- Print the postfix expression
+
+Example:
+Input:
+A+B*C
+
+Output:
+ABC*+
+
+Explanation:
+Operator precedence: * > +
+Use stack to handle operator precedence and associativity*/
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Push
+void push(char x) {
+    stack[++top] = x;
+}
+
+// Pop
+char pop() {
+    if (top == -1)
+        return -1;
+    return stack[top--];
+}
+
+// Peek
+char peek() {
+    if (top == -1)
+        return -1;
+    return stack[top];
+}
+
+// Precedence function
+int precedence(char op) {
+    if (op == '^') return 3;
+    if (op == '*' || op == '/') return 2;
+    if (op == '+' || op == '-') return 1;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i = 0, k = 0;
+
+    scanf("%s", infix);
+
+    while (infix[i] != '\0') {
+
+        // If operand → add to postfix
+        if (isalnum(infix[i])) {
+            postfix[k++] = infix[i];
+        }
+
+        // If '(' → push
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }
+
+        // If ')' → pop until '('
+        else if (infix[i] == ')') {
+            while (peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop(); // remove '('
+        }
+
+        // If operator
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(infix[i])) {
+                postfix[k++] = pop();
+            }
+            push(infix[i]);
+        }
+
+        i++;
+    }
+
+    // Pop remaining operators
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
