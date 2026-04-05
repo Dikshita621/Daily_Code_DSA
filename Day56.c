d65d4116db90059f8f85ace28e5c0ea486bb692a
Dikshita621 <dikshitachoudhary845@gmail.com>
2026-04-06 00:53:02 +0530

    Create Day56.c

---
+++ Day56.c
@@ -0,0 +1,103 @@
+/*Problem Statement:
+Check whether a given binary tree is symmetric around its center.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 indicates NULL)
+
+Output Format:
+- Print YES if symmetric, otherwise NO
+
+Example:
+Input:
+7
+1 2 2 3 4 4 3
+
+Output:
+YES
+
+Explanation:
+Left subtree is a mirror image of the right subtree.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create node
+struct Node* newNode(int data) {
+    struct Node* t = (struct Node*)malloc(sizeof(struct Node));
+    t->data = data;
+    t->left = t->right = NULL;
+    return t;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* root = newNode(arr[0]);
+    struct Node* q[n];
+    int f = 0, r = 0;
+
+    q[r++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct Node* cur = q[f++];
+
+        if (arr[i] != -1) {
+            cur->left = newNode(arr[i]);
+            q[r++] = cur->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            cur->right = newNode(arr[i]);
+            q[r++] = cur->right;
+        }
+        i++;
+    }
+    return root;
+}
+
+// Check mirror
+int isMirror(struct Node* t1, struct Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+// Check symmetric
+int isSymmetric(struct Node* root) {
+    if (root == NULL)
+        return 1;
+
+    return isMirror(root->left, root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    if (isSymmetric(root))
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
