0630f6f639128952adb0b00bfea86431d09a0bcb
Dikshita621 <dikshitachoudhary845@gmail.com>
2026-04-06 00:17:06 +0530

    Add BST implementation with search functionality
    Implement a Binary Search Tree (BST) with insertion and search functionalities.

---
+++ Day50.c
@@ -0,0 +1,73 @@
+/*Problem: BST Search
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create node
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return newNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node* root, int key) {
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+    else if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    if (search(root, key))
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
