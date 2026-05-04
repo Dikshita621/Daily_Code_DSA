b38675a951a33bab220845c2eb2036b3e57c137b
Dikshita Choudhary <dikshitachoudhary845@gmail.com>
2026-05-04 10:53:08 +0530

    Create Day96.c

---
+++ Day96.c
@@ -0,0 +1,62 @@
+/*Problem: Count number of inversions using modified merge sort.
+Inversion if i < j and a[i] > a[j].*/
+#include <stdio.h>
+
+#define MAX 100
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left;      // left subarray
+    int j = mid + 1;   // right subarray
+    int k = left;
+    long long inv_count = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i + 1);  // inversion count
+        }
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+
+    return inv_count;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long inv_count = 0;
+
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        inv_count += mergeSort(arr, temp, left, mid);
+        inv_count += mergeSort(arr, temp, mid + 1, right);
+        inv_count += merge(arr, temp, left, mid, right);
+    }
+
+    return inv_count;
+}
+
+int main() {
+    int n, arr[MAX], temp[MAX];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    long long inversions = mergeSort(arr, temp, 0, n - 1);
+
+    printf("%lld\n", inversions);
+
+    return 0;
+}
