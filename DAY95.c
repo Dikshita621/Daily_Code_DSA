c255af380798c9d0e437ab8dbe19a967e89ba486
Dikshita Choudhary <dikshitachoudhary845@gmail.com>
2026-06-16 22:49:12 +0530

    Update DAY95.c

--- DAY95.c
+++ DAY95.c
@@ -8,7 +8,7 @@ Distribute into buckets, sort each, concatenate.*/
 void insertionSort(float arr[], int n) {
     for (int i = 1; i < n; i++) {
         float key = arr[i];
-        int j = i - 1;
+        int j = i - 1:
 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
